c143287398b56f5463856118cba357aa26b08bef
Roberto Madinyá <roberto.madinya@ioet.com>
2022-09-25 16:39:21 -0500

    testing cd

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
+import logging
+import json
 import requests
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 def handler(event, context):
-    req_response = requests.get('https://api.github.com')
-    return req_response.json()
+    request_response = requests.get('https://api.github.com')
+    response = request_response.json()
+    return {"statusCode": 200, "body": json.dumps(response)}
